6f39b08875792e0f2e7149a8c47f73caa8226018
luffyUnleashed <37274365+luffyUnleashed@users.noreply.github.com>
2020-04-04 08:19:28 +0300

    clearing starting frish

--- vevn/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\musa\desktop\finalprojectloaymusa\gradproject\vevn\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
