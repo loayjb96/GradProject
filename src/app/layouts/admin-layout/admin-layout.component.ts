e91d21a4f054294f88d4805b756040bd1fcf5037
loayjb96 <50362994+loayjb96@users.noreply.github.com>
2020-04-11 15:53:55 +0300

    qweqwe

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -49,8 +49,8 @@ export class AdminLayoutComponent implements OnInit {
          }
       });
       this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
-           elemMainPanel.scrollTop = 0;
-           elemSidebar.scrollTop = 0;
+        //    elemMainPanel.scrollTop = 0;
+        //    elemSidebar.scrollTop = 0;
       });
       if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
           let ps = new PerfectScrollbar(elemMainPanel);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase:{
+
+    apiKey: "AIzaSyBskyZ52RbHnlDt3hvZubdGfd01AAQmUV8",
+    authDomain: "systemtestproject-96b42.firebaseapp.com",
+    databaseURL: "https://systemtestproject-96b42.firebaseio.com",
+    projectId: "systemtestproject-96b42",
+    storageBucket: "systemtestproject-96b42.appspot.com",
+    messagingSenderId: "511431138531",
+    appId: "1:511431138531:web:6e9d81ae269a7937efb9d6",
+    measurementId: "G-VBCVKD6CF5"
+
+  // Initialize Firebase
+  },
 };
