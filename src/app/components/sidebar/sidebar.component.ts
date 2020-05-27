277a346545972d01f7f1ec08dd008294e5e5bf21
loayjb96 <50362994+loayjb96@users.noreply.github.com>
2020-05-27 16:32:01 +0300

    working on authintication

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -3,6 +3,7 @@ import { Router, RouterModule ,NavigationEnd} from '@angular/router';
 import { AuthService } from 'app/auth/login/auth.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 declare const $: any;
 declare interface RouteInfo {
@@ -36,26 +37,40 @@ export const ROUTES: RouteInfo[] = [
 
 export class SidebarComponent implements OnInit {
   menuItems: any[];
-  role: any=" ";
+  role: any;
 
+  userCollection;
+  user: Observable<any[]> ;
   constructor(private router: Router,private db:AngularFirestore,public authService: AuthService,private af:AngularFireAuth) { }
-
   ngOnInit() {
+   
     let uid=this.authService.getToken()
+    this.userCollection = this.db.collection<any>('users')
+    this.user = this.userCollection.valueChanges()
+    this.user.subscribe(value=>{for(let i=0;i<value.length;i++){
 
-    this.menuItems = ROUTES.filter(menuItem => menuItem);
+ if(value[i].Uid==uid)
+ this.assign(value[i].Role )
+
+ this.menuItems = ROUTES.filter(menuItem => menuItem);
+
+    }
    
-    this.db.collection("users").doc(uid).get().toPromise().then(result => {
  
-      const actualData = result.data();
-      this.role=actualData.Role;
-      console.log(this.role)
-   
-
-  })
-
-  }
+    })
   
+   
+    
+
+   
+   
+  
+ 
+  }
+  assign(role){
+    console.log(role)
+    this.role=role
+  }
   isMobileMenu() {
       if ($(window).width() > 991) {
           return false;
